acaaa6a5fe8dd5ca1df60f614c49a74ab6374fec
Pazim Goyal <45088650+PazimGoyal@users.noreply.github.com>
2026-07-21 13:02:54 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
 }
-
 android {
     namespace = "com.pazim.smsemailforwarder"
     compileSdk = 36
@@ -13,8 +12,17 @@ android {
         versionCode = 5
         versionName = "5.0"
     }
-}
 
+    packaging {
+        resources {
+            excludes += "META-INF/LICENSE.md"
+            excludes += "META-INF/LICENSE-notice.md"
+            excludes += "META-INF/NOTICE.md"
+            excludes += "META-INF/NOTICE"
+            excludes += "META-INF/LICENSE"
+        }
+    }
+}
 dependencies {
     implementation("com.sun.mail:android-mail:1.6.7")
     implementation("com.sun.mail:android-activation:1.6.7")
