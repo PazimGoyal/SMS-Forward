9e724a37222763d6e78b26290b0147deb211260f
Pazim Goyal <45088650+PazimGoyal@users.noreply.github.com>
2026-07-21 00:28:25 -0400

    Update Config.java

--- app/src/main/java/com/pazim/smsemailforwarder/Config.java
+++ app/src/main/java/com/pazim/smsemailforwarder/Config.java
@@ -1,2 +1,2 @@
 package com.pazim.smsemailforwarder;
-public final class Config { private Config() {} public static final String WEB_APP_URL="https://script.google.com/macros/s/AKfycbxDvOYlSmerhxfH5uKaPYOo880Faljb0bhf2B4HhhQoUYjAexCQ3kTfCY977St4KHEQ3Q/exec"; public static final String SHARED_SECRET="pazimgoyal123123pazimgoyal"; }
+public final class Config { private Config() {} public static final String WEB_APP_URL="https://script.google.com/macros/s/AKfycbyn1CVzcUb_yCn8H73n0sa7bVw49MluVEN_z8Mut6EgGSJmF0PUN_omHSbyqcdo8bdxog/exec"; public static final String SHARED_SECRET="pazimgoyal123123pazimgoyal"; }
