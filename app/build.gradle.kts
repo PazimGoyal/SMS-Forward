cbe1191cf7828cfc97d5084d534aa79188fd30bf
Pazim Goyal <45088650+PazimGoyal@users.noreply.github.com>
2026-07-21 12:58:12 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,21 @@
-plugins { id("com.android.application") }
-android { namespace = "com.pazim.smsemailforwarder"; compileSdk = 36
- defaultConfig { applicationId = "com.pazim.smsemailforwarder"; minSdk = 23; targetSdk = 36; versionCode = 2; versionName = "2.0" } }
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.pazim.smsemailforwarder"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.pazim.smsemailforwarder"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 5
+        versionName = "5.0"
+    }
+}
+
+dependencies {
+    implementation("com.sun.mail:android-mail:1.6.7")
+    implementation("com.sun.mail:android-activation:1.6.7")
+}
