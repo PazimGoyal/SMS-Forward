10f5f20677a892592ed2202b08dc3bae570e4266
Pazim Goyal <45088650+PazimGoyal@users.noreply.github.com>
2026-07-21 00:10:45 -0400

    Update Config.java

--- app/src/main/java/com/pazim/smsemailforwarder/Config.java
+++ app/src/main/java/com/pazim/smsemailforwarder/Config.java
@@ -1,2 +1,2 @@
 package com.pazim.smsemailforwarder;
-public final class Config { private Config() {} public static final String WEB_APP_URL="PASTE_YOUR_GOOGLE_APPS_SCRIPT_EXEC_URL_HERE"; public static final String SHARED_SECRET="CHANGE_THIS_TO_A_LONG_RANDOM_SECRET"; }
+public final class Config { private Config() {} public static final String WEB_APP_URL="https://script.google.com/macros/s/AKfycbxDvOYlSmerhxfH5uKaPYOo880Faljb0bhf2B4HhhQoUYjAexCQ3kTfCY977St4KHEQ3Q/exec"; public static final String SHARED_SECRET="pazimgoyal123123pazimgoyal"; }
